70ae54b3a6dec79e05b550f014e8cf1022986053
N1Mee <81901402+N1Mee@users.noreply.github.com>
2022-06-05 13:49:49 +0300

    js upd
    05.06.22

---
+++ main.js
@@ -0,0 +1,29 @@
+let btnArr = document.querySelectorAll('.btnstyle a');
+let catalogLineArr = document.querySelectorAll('.catalog_line_product');
+
+console.log(btnArr);
+// console.log(catalogLineArr);
+
+for(let i = 0;i<btnArr.length;i++) {
+    btnArr[i].addEventListener('click' , (event) => {
+        let id = event.target.id;
+        console.log(id)
+        openCatalog(id);
+    })
+}
+
+
+
+function  openCatalog(id) {
+    catalogLineArr.forEach(item => {
+        item.style.display = 'none';
+        
+        console.log(`${id}-line`);
+
+        if(item.classList.contains(`${id}-line`)) {
+            item.style.display = 'flex'
+        }
+
+
+        });
+} 
